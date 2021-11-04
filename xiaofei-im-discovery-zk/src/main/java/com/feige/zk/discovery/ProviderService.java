80d3f448c90cd6e6a59be50db308456fcc4e908e
胡飞 <1835698775@qq.com>
2021-11-04 23:34:51 +0800

    初始化厂库

---
+++ xiaofei-im-discovery-zk/src/main/java/com/feige/zk/discovery/ProviderService.java
@@ -0,0 +1,10 @@
+package com.feige.zk.discovery;
+
+/**
+ * @author feige<br />
+ * @ClassName: ProviderService <br/>
+ * @Description: <br/>
+ * @date: 2021/11/4 23:26<br/>
+ */
+public interface ProviderService {
+}
---
+++ xiaofei-im-discovery-zk/src/main/java/com/feige/zk/discovery/impl/ProviderServiceImpl.java
@@ -0,0 +1,10 @@
+package com.feige.zk.discovery.impl;
+
+/**
+ * @author feige<br />
+ * @ClassName: ProviderServiceImpl <br/>
+ * @Description: <br/>
+ * @date: 2021/11/4 23:27<br/>
+ */
+public class ProviderServiceImpl {
+}
