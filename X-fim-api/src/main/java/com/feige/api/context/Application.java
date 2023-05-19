99ca2d32c64c12eb83ece99f9bcf6b134f191bf1
胡飞 <1835698775@qq.com>
2023-05-19 18:08:34 +0800

    应用开发

--- X-fim-api/src/main/java/com/feige/api/context/Application.java
+++ X-fim-api/src/main/java/com/feige/api/context/Application.java
@@ -4,10 +4,5 @@ package com.feige.api.context;
 public interface Application extends Lifecycle {
     
 
-    /**
-     * Running state
-     * @return Whether to run
-     */
-    boolean isRunning();
 
 }
---
+++ X-fim-api/src/main/java/com/feige/api/context/Environment.java
@@ -0,0 +1,14 @@
+package com.feige.api.context;
+
+import com.feige.api.config.Config;
+
+public class Environment extends LifecycleAdapter implements Application {
+    
+    private Config config;
+    
+    @Override
+    public void initialize(){
+        
+    }
+    
+}
