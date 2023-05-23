fcf5ec0973226aeb0425115900c428fea71f92b8
胡飞 <1835698775@qq.com>
2023-05-23 23:02:36 +0800

    handler

--- X-fim-api/src/main/java/com/feige/api/handler/IHandler.java
+++
@@ -1,8 +0,0 @@
-package com.feige.api.handler;
-
-import com.feige.api.session.Session;
-
-public interface IHandler {
-    
-    void handle(Session session, Object msg);
-}
---
+++ X-fim-api/src/main/java/com/feige/api/handler/MsgHandler.java
@@ -0,0 +1,19 @@
+package com.feige.api.handler;
+
+import com.feige.api.session.Session;
+
+public interface MsgHandler {
+
+    /**
+     * handle
+     * @param session session
+     * @param msg msg
+     */
+    void handle(Session session, Object msg);
+
+    /**
+     * cmd
+     * @return cmd
+     */
+    int getCmd();
+}
