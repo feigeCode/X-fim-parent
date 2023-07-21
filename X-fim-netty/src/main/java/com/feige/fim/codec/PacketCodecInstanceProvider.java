2e38310693b20c2d9b06aeb947998fcf592be7ab
胡飞 <1835698775@qq.com>
2023-07-21 09:27:28 +0800

    ioc改造

--- X-fim-netty/src/main/java/com/feige/fim/codec/PacketCodecInstanceProvider.java
+++ X-fim-netty/src/main/java/com/feige/fim/codec/PacketCodecInstanceProvider.java
@@ -4,20 +4,20 @@ import com.feige.api.annotation.Spi;
 import com.feige.api.annotation.Value;
 import com.feige.api.codec.Codec;
 import com.feige.api.spi.InstanceProvider;
-import com.feige.fim.config.Configs;
+import com.feige.fim.config.Configs.ConfigKey;
 
 @Spi("packet")
 public class PacketCodecInstanceProvider implements InstanceProvider<Codec> {
     
-    @Value(Configs.ConfigKey.CODEC_MAX_PACKET_SIZE_KEY)
+    @Value(ConfigKey.CODEC_MAX_PACKET_SIZE_KEY)
     private int maxPacketSize;
-    @Value(Configs.ConfigKey.CODEC_HEARTBEAT_KEY)
+    @Value(ConfigKey.CODEC_HEARTBEAT_KEY)
     private byte heartbeat;
-    @Value(Configs.ConfigKey.CODEC_VERSION_KEY)
+    @Value(ConfigKey.CODEC_VERSION_KEY)
     private byte version;
-    @Value(Configs.ConfigKey.CODEC_HEADER_LENGTH_KEY)
+    @Value(ConfigKey.CODEC_HEADER_LENGTH_KEY)
     private int headerLength;
-    @Value(Configs.ConfigKey.CODEC_CHECK_SUM_KEY)
+    @Value(value = ConfigKey.CODEC_CHECK_SUM_KEY, nullSafe = false)
     private String checkSumKey;
     
     @Override
