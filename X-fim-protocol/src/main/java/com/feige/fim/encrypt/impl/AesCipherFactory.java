713351208445fbc915feee83c12449cb073b9f98
胡飞 <1835698775@qq.com>
2023-07-22 23:31:47 +0800

    ioc改造

--- X-fim-protocol/src/main/java/com/feige/fim/encrypt/impl/AesCipherFactory.java
+++ X-fim-protocol/src/main/java/com/feige/fim/encrypt/impl/AesCipherFactory.java
@@ -1,9 +1,11 @@
 package com.feige.fim.encrypt.impl;
 
+import com.feige.api.annotation.SpiComp;
 import com.feige.api.cipher.Cipher;
 import com.feige.api.cipher.CipherFactory;
 
 
+@SpiComp("aes")
 public class AesCipherFactory  implements CipherFactory {
     @Override
     public Cipher create(String key, String... args) throws Exception {
--- X-fim-protocol/src/main/java/com/feige/fim/encrypt/impl/RsaCipherFactory.java
+++ X-fim-protocol/src/main/java/com/feige/fim/encrypt/impl/RsaCipherFactory.java
@@ -1,8 +1,10 @@
 package com.feige.fim.encrypt.impl;
 
+import com.feige.api.annotation.SpiComp;
 import com.feige.api.cipher.Cipher;
 import com.feige.api.cipher.CipherFactory;
 
+@SpiComp("rsa")
 public class RsaCipherFactory implements CipherFactory {
     @Override
     public Cipher create(String key, String... args) {
