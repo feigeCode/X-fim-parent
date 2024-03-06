62d215ce1a64ed1802c6f9ce8f5dd9a58311d546
胡飞 <1835698775@qq.com>
2024-03-06 22:55:27 +0800

    feat: 调整工程名称

--- fim-common/src/main/java/com/feige/api/session/NettySessionRepository.java
+++ fim-common/src/main/java/com/feige/api/session/NettySessionRepository.java
@@ -1,8 +1,6 @@
-package com.feige.fim.adapter;
+package com.feige.api.session;
 
 import com.feige.utils.spi.annotation.SPI;
-import com.feige.api.session.SessionRepository;
-import com.feige.api.session.AbstractSessionRepository;
 
 
 @SPI(value="nettySessionRepository", interfaces = SessionRepository.class)
