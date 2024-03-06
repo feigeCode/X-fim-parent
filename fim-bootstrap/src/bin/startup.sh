f78f7d784d53fd1f3f74c0ca7b1bf9709cd8e210
胡飞 <1835698775@qq.com>
2024-03-06 19:15:37 +0800

    feat: 日志优化 新增log4j2日志

--- fim-bootstrap/src/bin/startup.sh
+++ fim-bootstrap/src/bin/startup.sh
@@ -3,7 +3,8 @@ currentPath=$(cd `dirname $0`; pwd)
 cd "$currentPath"
 
 fim.path=$currentPath"/../conf/fim.yaml"
+log4j.configurationFile=$currentPath"/../conf/log4j2.xml"
 export fim.path
-
+export log4j.configurationFile
 
 java -jar $currentPath"/../fim-bootstrap-1.0.jar"
